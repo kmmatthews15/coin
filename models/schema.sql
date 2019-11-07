d527b4b4e37b60f32331c36f861983b3daa845f9
Keisha <kmmatthews15@gmail.com>
2019-11-07 13:18:31 -0600

    added the first coins db

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,10 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE coindb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE coindb;
+
+CREATE TABLE coins (
+	id INT AUTO_INCREMENT NOT NULL, 
+    description VARCHAR(255),
+    createdAT TIMESTAMP NOT NULL, 
+    PRIMARY KEY(id)
+    );
