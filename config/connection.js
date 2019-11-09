98243d5f662bc47fda7f4aa61d76a6cfd6191d5d
tardyparty <tardyparty@users.noreply.github.com>
2019-11-09 10:32:19 -0600

    nothing

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,15 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost2',
-        user: 'root',
-        password: 'Joon912!!',
-        database: 'coin_db'
-    });
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: 'localhost2',
+    user: 'root',
+    password: 'Joon912!!',
+    database: 'coin_db'
+  });
 };
 
 connection.connect();
